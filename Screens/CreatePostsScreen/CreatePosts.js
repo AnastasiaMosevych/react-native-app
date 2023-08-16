63a048d26ea07372bfc603a4bfa179b9f0cfcb23
AnastasiaMosevych <ukrainischesmaedel@yahoo.de>
2023-08-16 17:47:14 +0200

    deleted unnecessary code

--- Screens/CreatePostsScreen/CreatePosts.js
+++ Screens/CreatePostsScreen/CreatePosts.js
@@ -118,41 +118,7 @@ const styles = StyleSheet.create({
     trashIcon: {
         color: '#BDBDBD',
         alignSelf: 'center',
-    }
-    //     container: { flex: 1 },
-    //     camera: { flex: 1 },
-    //     photoView: {
-    //         flex: 1,
-    //         backgroundColor: "transparent",
-    //         justifyContent: "flex-end",
-    //     },
-
-    //   flipContainer: {
-    //     flex: 0.1,
-    //     alignSelf: "flex-end",
-    //   },
-
-    //   button: { alignSelf: "center" },
-
-    //   takePhotoOut: {
-    //     borderWidth: 2,
-    //     borderColor: "white",
-    //     height: 50,
-    //     width: 50,
-    //     display: "flex",
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //     borderRadius: 50,
-    //   },
-
-    //   takePhotoInner: {
-    //     borderWidth: 2,
-    //     borderColor: "white",
-    //     height: 40,
-    //     width: 40,
-    //     backgroundColor: "white",
-    //     borderRadius: 50,
-    //   },
+    },
 });
 
 export default CreatePostsScreen;
